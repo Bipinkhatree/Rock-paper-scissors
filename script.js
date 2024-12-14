e71b53968a629f0c9606a91c01212edde3ff162d
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-14 18:45:44 +0545

    Finish building the UI and logic for single Round

--- script.js
+++ script.js
@@ -8,25 +8,13 @@ function getCompChoice() {
     return choice;
 }
 
-let compChoice = getCompChoice(); //CALL getCompChoice to generate a computer choice and store in compChoice
-console.log(compChoice);
 
-//Creating a function where user can input his/her choice
-function getHumanChoice() {
-    choices = ['rock', 'paper', 'scissor']
-    input = prompt("please enter your choice : 0:'rock' 1:'paper' 2:'scissor'");// Telling user to enter choice
-    userChoice = choices[input];
-    console.log(userChoice);
-    //Checking if the user inputs the valid choice
-    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissor') {
-        console.log(`You have choosed ${userChoice}`)
-    }
-    else {
-        console.log("Please input a valid choice"); //If user inputs invalid choice inform them
-    }
-    return userChoice;
-}
-let humanChoice = getHumanChoice(); //CALL humanChoice to get user choice
+//Selecting elements through querySelectors
+
+let buttons = document.querySelectorAll("button");
+
+
+//CALL humanChoice to get user choice
 
 
 //Creating variables to store scores
@@ -35,9 +23,9 @@ let compScore = 0;
 let userScore = 0;
 
 //IF-THEN-ELSE
-function playRound(compChoice, humanChoice) {
-
-
+function playRound(e) {
+    let compChoice = getCompChoice();
+    let humanChoice = e.target.id;
     if (compChoice === humanChoice) {
         drawGame(compChoice, humanChoice, compScore, userScore);
     }
@@ -71,8 +59,7 @@ function userWin(compChoice, humanChoice, compScore, userScore) {
     console.log(`your score: ${userScore} Computer score: ${compScore} `)
 }
 
-
-playRound(compChoice, humanChoice);
+buttons.forEach(button => button.addEventListener("click", playRound));
 
 
 // playGame();
