71b0ec3452644a784b0b7fd774dca31a7032903d
Bipin Khatri <159134672+Bipinkhatree@users.noreply.github.com>
2024-12-14 19:26:11 +0545

    Finished the final tweaks and logic for game until score is reached 5

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function getCompChoice() {
     choices = ['rock', 'paper', 'scissor']; //Array of choices
     randNum = Math.floor(Math.random() * 3); //Generates a random number from 0 - 2
     choice = choices[randNum]; //selects one choice out of 3 from choices 
-    console.log(randNum);
-    console.log(choice);
     return choice;
 }
 
@@ -13,8 +11,10 @@ function getCompChoice() {
 
 let buttons = document.querySelectorAll("button");
 
+let winner = document.querySelector("#winner");
 
-//CALL humanChoice to get user choice
+let score = document.querySelector("#score");
+
 
 
 //Creating variables to store scores
@@ -24,42 +24,67 @@ let userScore = 0;
 
 //IF-THEN-ELSE
 function playRound(e) {
-    let compChoice = getCompChoice();
-    let humanChoice = e.target.id;
+    winner.textContent = "";
+
+    score.textContent = "";
+
+    let compChoice = getCompChoice(); //getting computerChoice through the getCompChoice() function
+
+    let humanChoice = e.target.id;  //getting humanchoice through the eventlistner
+
+
     if (compChoice === humanChoice) {
         drawGame(compChoice, humanChoice, compScore, userScore);
     }
+
+
     else if ((compChoice === 'rock' && humanChoice === 'scissor') || (compChoice === 'paper' && humanChoice === 'rock') || (compChoice === 'scissor' && humanChoice === 'paper')) {
         compWin(compChoice, humanChoice, compScore, userScore);
     }
+
+
     else {
         userWin(compChoice, humanChoice, compScore, userScore);
     }
 
+    //check if any player has reached 5 points
+
+    if (compScore >= 5 || userScore >= 5) {
+        buttons.forEach(button => button.removeEventListener("click", playRound)); //stop the game
+        winner.textContent = compScore >= 5 ? "Computer wins the game" : "You win the game!";
+    }
+
 }
 
+
 //result after drawGame
-function drawGame(compChoice, humanChoice, compScore, userScore) {
-    console.log(`It's a draw. Computer choose ${compChoice} and you choose ${humanChoice}`);
+function drawGame(compChoice, humanChoice) {
+    winner.textContent += `It's a draw. Computer choose ${compChoice} and you choose ${humanChoice}`;
     compScore++;
     userScore++;
-    console.log(`your score: ${userScore} Computer score: ${compScore} `)
+    score.textContent += `your score: ${userScore} Computer score: ${compScore} `;
 
 }
 //Results after computer win
-function compWin(compChoice, humanChoice, compScore, userScore) {
-    console.log(`Computer Wins. Computer choose ${compChoice} and you choose ${humanChoice}`);
+function compWin(compChoice, humanChoice) {
+    winner.textContent += `Computer Wins.Computer choose ${compChoice} and you choose ${humanChoice}`;
     compScore++;
-    console.log(`your score: ${userScore} Computer score: ${compScore} `)
+    score.textContent += `your score: ${userScore} Computer score: ${compScore}`;
 }
 //Results after user win
-function userWin(compChoice, humanChoice, compScore, userScore) {
-    console.log(`You Win. Computer choose ${compChoice} and you choose ${humanChoice}`);
+function userWin(compChoice, humanChoice) {
+    winner.textContent += `You Win.Computer choose ${compChoice} and you choose ${humanChoice}`;
     userScore++;
-    console.log(`your score: ${userScore} Computer score: ${compScore} `)
+    score.textContent += `your score: ${userScore} Computer score: ${compScore} `;
 }
-
-buttons.forEach(button => button.addEventListener("click", playRound));
 
 
 // playGame();
+
+if (compScore < 5 || userScore < 5) {
+    buttons.forEach(button => button.addEventListener("click", playRound));
+}
+
+
+
+
